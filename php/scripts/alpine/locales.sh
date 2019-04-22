8540e68b6315da749711dc6698a0aa1c11bd1c9f
Wiljan Slofstra <wiljanslofstra@gmail.com>
2019-04-22 16:10:19 +0200

    Move locale.md into tmp folder for use]

--- php/scripts/alpine/locales.sh
+++ php/scripts/alpine/locales.sh
@@ -8,7 +8,4 @@ wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerr
     apk add glibc-bin-2.29-r0.apk glibc-i18n-2.29-r0.apk glibc-2.29-r0.apk
 
 # Iterate through all locale and install it
-# Note that locale -a is not available in alpine linux, use `/usr/glibc-compat/bin/locale -a` instead
-cp ./locale.md /locale.md
-
-cat /locale.md | xargs -i /usr/glibc-compat/bin/localedef -i {} -f UTF-8 {}.UTF-8
+cat /tmp/locale.md | xargs -i /usr/glibc-compat/bin/localedef -i {} -f UTF-8 {}.UTF-8
